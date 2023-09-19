c28036c60d61f91cfbdc0e0af7ef38f701dd0abf
Noneku <dev.nassim.pro@gmail.com>
2023-09-19 15:10:14 +0200

    Login Work with redirection + form Register Client

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\ClientRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Client implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]

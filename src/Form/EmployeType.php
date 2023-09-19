04850a6d54c18ff156733125f7bf9d2df4f69d91
User <nicolas.deweireld@gmail.com>
2023-09-19 14:22:40 +0200

    CRUD employes

---
+++ src/Form/EmployeType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employe;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('matricule')
+            ->add('plainPassword',TextType::class,[
+                'mapped'=>false
+                ])
+            ->add('nom')
+            ->add('prenom')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Employe::class,
+        ]);
+    }
+}

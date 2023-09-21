69273d03bde0fca00f6641943df59ab56e499268
nicodew2 <nicolas.deweireld@gmail.com>
2023-09-21 10:43:59 +0200

    chgt de nom

--- src/Controller/SecurityEmployeController.php
+++ src/Controller/SecurityEmployeController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityEmployeController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login_employe')]
+    #[Route(path: '/login_employe', name: 'app_login_employe')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ($this->getUser()) {
@@ -21,7 +21,7 @@ class SecurityEmployeController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render('security/login_employe.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 
     #[Route(path: '/logout', name: 'app_logout')]

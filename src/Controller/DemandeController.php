21c615a766fe2217dafb31a69d9bab00f7651ea0
None.ku <dev.nassim.pro@gmail.com>
2023-09-22 11:35:57 +0200

    Resolve duplicate keys

--- src/Controller/DemandeController.php
+++ src/Controller/DemandeController.php
@@ -2,11 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Prestation;
 use App\Entity\Demande;
 use App\Form\DemandeType;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,7 +17,7 @@ class DemandeController extends AbstractController
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {   
        // Obtenez toutes les prestations
-       $prestations = $entityManager->getRepository(Prestation::class)->findAll();
+    //    $prestations = $entityManager->getRepository(Prestation::class)->findAll();
 
 
        // Vérifiez si l'utilisateur est connecté
@@ -28,28 +27,33 @@ class DemandeController extends AbstractController
        $user = $this->getUser();
 
        // Créez une nouvelle instance de l'entité Demande
-    //    $demande = new Demande();
+       $demande = new Demande();
 
-    //    // Créez le formulaire en utilisant le formulaire DemandeType
-    //    $form = $this->createForm(DemandeType::class, $demande, [
-    //        'prestations' => $prestations, // Passez les prestations au formulaire
-    //    ]);
+       // Créez le formulaire en utilisant le formulaire DemandeType
+       $form = $this->createForm(DemandeType::class, $demande);
+        
+       // Gérez la soumission du formulaire
+       $form->handleRequest($request);
+       if ($form->isSubmitted() && $form->isValid()) {
 
-    //    // Gérez la soumission du formulaire
-    //    $form->handleRequest($request);
-    //    if ($form->isSubmitted() && $form->isValid()) {
-    //        // Traitez les données du formulaire ici, par exemple, enregistrez-les en base de données
-    //     //    $entityManager->persist($demande);
-    //     //    $entityManager->flush();
+        $entityPrestation = $form->get('prestation')->getData();
+        $dateDemande  = new DateTime();
 
-    //        // Redirigez vers une autre page ou effectuez une autre action
-    //        return $this->redirectToRoute('votre_autre_route');
-    //    }
+        // var_dump($entityPrestation);
+           $demande->setClient($user);
+           $demande->setPrestation($entityPrestation);
+           $demande->setDateDemande($dateDemande);
+           $entityManager->persist($demande);
+           $entityManager->flush();
+
+           // Redirigez vers une autre page ou effectuez une autre action
+           return $this->redirectToRoute('/');
+       }
           
        return $this->render('demande/index.html.twig', [
            'controller_name' => 'DemandeController',
            'user' => $user,
-        //    'form' => $form->createView(), // Créez la vue du formulaire
+           'form' => $form
        ]);
    }
 }

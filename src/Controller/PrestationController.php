d8a8287aeda5837310e051fdb2bc6bfbd32721b2
None.ku <dev.nassim.pro@gmail.com>
2023-09-28 14:30:54 +0200

    Integration Front

--- src/Controller/PrestationController.php
+++ src/Controller/PrestationController.php
@@ -6,13 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeClientController extends AbstractController
+class PrestationController extends AbstractController
 {
-    #[Route('/home/client', name: 'app_home_client')]
+    #[Route('/prestations', name: 'app_prestations')]
     public function index(): Response
     {
-        return $this->render('home_client/index.html.twig', [
-            'controller_name' => 'HomeClientController',
+        return $this->render('prestations/index.html.twig', [
+            'controller_name' => 'PrestationController',
         ]);
     }
 }

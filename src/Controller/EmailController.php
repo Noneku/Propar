9d8485ac23747cc700dd06c1d2a2735446824f22
nicodew2 <nicolas.deweireld@gmail.com>
2023-10-10 10:21:07 +0200

    recup dev

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -9,7 +9,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
-use Symfony\Component\Mailer\Transport;
+
 
 class EmailController extends AbstractController
 {
